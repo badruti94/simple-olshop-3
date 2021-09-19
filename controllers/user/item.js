b6f1fe07b775e761c25baa87e24960525e7e178a
Badru <badruakfm@gmail.com>
2021-09-19 10:28:08 +0700

    FEAT(User:Item) : Add pagination feature

--- controllers/user/item.js
+++ controllers/user/item.js
@@ -2,12 +2,29 @@ const {sequelize, Sequelize} = require('../../models')
 const Item = require('../../models/items')(sequelize, Sequelize.DataTypes)
 
 exports.getItem = async (req, res) =>  {
-    const items = await Item.findAll()
+    const {page} = req.query
+
+    const perPage = 10
+    const activePage = page ? parseInt(page) : 1
+
+    const totalData = await Item.count()
+    const lastPage = Math.ceil(totalData / perPage)
+    const index = (perPage * activePage) - perPage
+    const items = await Item.findAll({
+        offset: index,
+        limit : perPage
+    })
 
     res.status(200).json({
         status: 'success',
         data: {
-            items
+            items,
+            pagination: {
+                firstPage : 1,
+                lastPage,
+                prev: activePage - 1,
+                next : activePage + 1
+            }
         }
     })
 }
